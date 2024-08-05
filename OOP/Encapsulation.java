9fc375f080b516b84869bbcf448480c3c426f6f5
dnyaneshagale <150069504+dnyaneshagale@users.noreply.github.com>
2024-08-05 12:34:50 +0530

    No Such Significant Changes

--- OOP/Encapsulation.java
+++ OOP/Encapsulation.java
@@ -1,29 +1,29 @@
-class Human{
-     int age;       //Accessible only in this class
-     String name;  //Accessible only in This Class
+class Human {
+    private int age;       // Accessible only in this class
+    private String name;  // Accessible only in this class
 
-    public Human(){  //Constructor -> Same name as Class Name but as a Method.
-                    //Everytime an object is Created it will call Constructor
+    public Human() {  /* Constructor -> Same name as Class Name but as a Method.
+                        Every time an object is Created it will call Constructor */
         System.out.println("In Constructor");
 
         age = 19;
         name = "Dnyanesh";
     }
 
-
-    void setAge(int age){
-        this.age = age;  //'this' Keyword will Help to Assign values to the object which is caleed
+    void setAge(int age) {
+        this.age = age;  /* 'this' Keyword will Help to Assign values 
+                            to the object which is called */
     }
 
-    int getAge(){
+    int getAge() {
         return age;
     }
 
-    void setName(String name){
+    void setName(String name) {
         this.name = name;
     }
 
-    String getName(){
+    String getName() {
         return name;
     }
 }
---
+++ OOP/HelloWorld.java
@@ -0,0 +1,5 @@
+public interface HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
