eccecb90d91bcc6b35749eb5314cc6eef8bb1695
dnyaneshagale <150069504+dnyaneshagale@users.noreply.github.com>
2024-08-24 23:11:59 +0530

    Find the kth smallest element in the matrix LeetCode378

--- #LeetCode/3. Searching & Sorting/Leetcode378.java
+++ #LeetCode/3. Searching & Sorting/Leetcode378.java
@@ -18,4 +18,25 @@ class Solution {
         }
         return pq.poll();
     }
+
+/*
+ class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        
+        int[] ans = new int[n * n];
+
+        int l = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                ans[l] = matrix[i][j];
+                l++;
+            }
+        }
+        Arrays.sort(ans);
+        
+        return ans[k-1];
+    }
+}
+ */
 }
