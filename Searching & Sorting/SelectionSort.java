b4e89b8df2060fd3e1aaf896abf6383eefc0f9d2
dnyaneshagale <150069504+dnyaneshagale@users.noreply.github.com>
2024-09-10 23:42:48 +0530

    Few Changes in the code

--- Searching & Sorting/SelectionSort.java
+++ Searching & Sorting/SelectionSort.java
@@ -18,34 +18,19 @@ public class SelectionSort{
 
     public static void SelectionSort(int[] arr){
         for(int i = 0; i<arr.length-1; i++){
-            int mi = i; //mi -> Minimum Index
+            int minPos = i; //mi -> Minimum Index
             for(int j = i+1; j < arr.length; j++){
-                if (isSmaller(arr, j, mi)) {
-                    mi = j;
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
                 }
             }
-            swap(arr, i, mi);
+            //swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
         }
     }
 
-
-
-    public static boolean isSmaller(int[] arr, int i, int j){
-        System.out.println("Comparing "+ arr[i]+ " and " + arr[j]);
-        if(arr[i]<arr[j]){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public static void swap(int[] arr, int i, int j){
-        System.out.println("Swapping "+ arr[i]+ " and "+arr[j]);
-
-        int temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
 
     public static void print(int[] arr){
         for(int i = 0; i<arr.length; i++){
