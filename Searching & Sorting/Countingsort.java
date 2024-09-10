f9b2cf6d35658fc37d5c3d00b684e5633db32a90
dnyaneshagale <150069504+dnyaneshagale@users.noreply.github.com>
2024-09-11 00:20:21 +0530

    Count Sort

--- Searching & Sorting/Countingsort.java
+++ Searching & Sorting/Countingsort.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
+
 public class Countingsort {
-    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        countingSort(arr);
+        for(int val : arr) {
+            System.out.print(val);
+        }
+    }
+
+    public static void countingSort(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int[] count = new int[largest+1];
+        for(int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        //sorting
+        int j = 0;
+        for(int i = 0; i < count.length; i++) {
+            while(count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
 }
