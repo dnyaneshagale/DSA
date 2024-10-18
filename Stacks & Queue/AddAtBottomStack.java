f3c3be9cea9ab955a18bfaafd039e6bebdffdd3c
Dnyanesh Agale <dnyanesh0019@gmail.com>
2024-10-18 07:13:08 +0530

    Add at bottom fxn added

--- Stacks & Queue/AddAtBottomStack.java
+++ Stacks & Queue/AddAtBottomStack.java
@@ -12,16 +12,33 @@ public class AddAtBottomStack {
         addAtBottom(s, data);
         s.push(top);
     }
-    public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
 
-        addAtBottom(s, 4);
-
-        while(!s.isEmpty()){
-            System.out.println(s.pop());
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            s.push(str.charAt(i));
         }
+
+        StringBuilder sb = new StringBuilder("");
+        while (! s.isEmpty()) {
+            // char curr = s.pop();
+            sb.append(s.pop());
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // addAtBottom(s, 4);
+
+        // while(!s.isEmpty()){
+        //     System.out.println(s.pop());
+        // }
+        String str = "Dnyanesh";
+        String result = reverseString(str);
+        System.out.println(result);
     }
 }
