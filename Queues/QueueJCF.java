179dbd9b8e2b17cb55beb60e4dd965c3e03b67d3
Dnyanesh Agale <dnyanesh0019@gmail.com>
2024-10-21 22:30:18 +0530

    Queue Using Java Collections Framework

---
+++ Queues/QueueJCF.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.LinkedList;
+public class QueueJCF {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("Using LinkedList Class : ");
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+ /*
+  - Queue is an Interface in Java. 
+  - Interfaces cannot be Created as Objects
+  - so we use either LinkedList or ArrayDeque Class to create Queue.
+  */
+
+
+        q = new ArrayDeque<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("\nUsing ArrayDeque Class : ");
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+
+
+
+        System.out.println("  - Queue is an Interface in Java. \r\n" + //
+                        "  - Interfaces cannot be Created as Objects\r\n" + //
+                        "  - so we use either LinkedList or ArrayDeque Class to create Queue.");
+    }
+}
