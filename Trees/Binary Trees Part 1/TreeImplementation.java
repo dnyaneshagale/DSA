4ed897951fcd30860116c2b3c7abb01368bddb54
Dnyanesh Agale <dnyanesh0019@gmail.com>
2024-11-13 21:43:42 +0530

    Traversing The Tree

--- Trees/Binary Trees Part 1/TreeImplementation.java
+++ Trees/Binary Trees Part 1/TreeImplementation.java
@@ -27,11 +27,49 @@ public class TreeImplementation {
             return newNode;
         }
         
+        public static void preOrder(Node root) {
+            if(root == null) {
+                return;
+            }
+
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public static void inOrder(Node root) {
+            if(root == null) {
+                return;
+            }
+
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+
+        public static void postOrder(Node root) {
+            if(root == null) {
+                return;
+            }
+
+            postOrder(root.left);
+            preOrder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
     public static void main(String[] args) {
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
         BinaryTree tree = new BinaryTree();
-        System.out.println(tree.buildTree(nodes).data);
+        Node root = tree.buildTree(nodes);
+
+        System.out.println("PreOrder -> ");
+        tree.preOrder(root);
+
+        System.out.println("\n\nInOrder -> ");
+        tree.inOrder(root);
+        
+        System.out.println("\n\nPostOrder -> ");
+        tree.postOrder(root);
     }
 }
