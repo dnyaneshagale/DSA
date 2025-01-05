c4a6ba21ae135cce6942ef02526d2e582d329e20
Dnyanesh Agale <dnyanesh0019@gmail.com>
2025-01-05 23:31:43 +0530

    TreeSet Intro

---
+++ Hashing/TreeSetIntro.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class TreeSetIntro {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        ts.add(150);
+        ts.add(160);
+        ts.add(100);
+        ts.add(20);
+
+        System.out.println(ts);
+
+        // Sorted Order me milega
+    }
+}
