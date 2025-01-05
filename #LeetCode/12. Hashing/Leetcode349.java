9355fd85a658ef620b750d391468a7c240fc6f8c
Dnyanesh Agale <dnyanesh0019@gmail.com>
2025-01-06 01:03:14 +0530

    Intersection of Two Arrays

---
+++ #LeetCode/12. Hashing/Leetcode349.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+
+        for(int i = 0; i < nums2.length; i++) {
+            if(set.contains(nums2[i])) {
+                list.add(nums2[i]);
+                set.remove(nums2[i]);
+            }
+        }
+        
+        int[] ans = new int[list.size()];
+        int i = 0;
+        for(Integer val : list) {
+            ans[i] = val;
+            i++;
+        }
+        return ans;
+    }
+}
