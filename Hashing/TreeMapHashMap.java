548569841a4c8fc92d25f9b8abb17091cd599bc3
Dnyanesh Agale <dnyanesh0019@gmail.com>
2025-01-05 22:46:05 +0530

    Tree Map HashMap

---
+++ Hashing/TreeMapHashMap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class TreeMapHashMap {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> thm = new TreeMap();
+
+        thm.put("India", 150);
+        thm.put("China", 160);
+        thm.put("Pakistan", 100);
+        thm.put("Japan", 20);
+
+        System.out.println(thm);
+
+        // Keys are Sorted 
+    }
+}
