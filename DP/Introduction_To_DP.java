342d2502a086271fe46be74a38673bb3998a7201
Dnyanesh Agale <dnyanesh0019@gmail.com>
2025-01-17 17:57:35 +0530

    Introduction to Dynamic Programming

---
+++ DP/Introduction_To_DP.java
@@ -0,0 +1,38 @@
+package DP;
+
+public class Introduction_To_DP {
+    /*
+     * 
+     * It is Optimized recursion
+     * 
+     *  + How to identify
+     *  - 1. Asked for Optimal Solution (Least, Most, Min, Max, Smallest, Largest)
+     *  
+     *  - 2. Some choice is given. (It may form multiple branches in Recursion Tree type of thing)
+     * 
+     * 
+     *  + Definition : (Tell in Interview)
+     *  - Dynamic Programming is a technique in computer programming, that helps to efficiently solve a 
+     *      class of problems that have "Overlapping Subproblems" & "Optimal Substructure" property.
+     * 
+     *  + Ways of DP :
+     *  - 1. Memoization (Top-Down Approach).
+     *  - 2. Tabulation (Bottom-Up Approach).
+     * 
+     *  + 1. Memoization :
+     *     I. Normal Recursion
+     *    II. Subproblems
+     *   III. Storage -> Reuse
+     * 
+     *  
+     *  + 2. Tabulation :
+     *     I. Efficiemt
+     *    II. Iteration is Used.
+     *   III. Table/Set/Storage
+     *  Steps : 
+     *      1. Initialization
+     *      2. Meaning
+     *      3. Filling (Small to Large). 
+     * 
+     */
+}
