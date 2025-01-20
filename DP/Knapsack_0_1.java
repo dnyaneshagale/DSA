cd92fd2369c2bb46e83b248e437cb5e32c7cbad7
Dnyanesh Agale <dnyanesh0019@gmail.com>
2025-01-20 21:20:51 +0530

    Knapsack in Tabulation Approach

--- DP/Knapsack_0_1.java
+++ DP/Knapsack_0_1.java
@@ -47,6 +47,42 @@ public class Knapsack_0_1 {
             return dp[n][W];
         }
     }
+
+
+    // Tabulation Technique
+
+    public static int knapsackTab(int[] val, int[] wt, int W) {
+        int n = val.length;
+        int[][] dp = new int[n+1][W+1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                int v = val[i-1];
+                int w = wt[i-1];
+
+                if (w <= j) {
+                    int incrProf = v + dp[i-1][j-w];
+                    int exclProf = dp[i-1][j];
+
+                    dp[i][j] = Math.max(incrProf, exclProf);
+                } else {
+                    int exclProf = dp[i-1][j];
+                    dp[i][j] = exclProf;
+                }
+            }
+        }
+        return dp[n][W];
+    }
+    
     public static void main(String[] args) {
         int[] val = {15, 14, 10, 45, 30};
         int[] wt = {2, 5, 1, 3, 4};
@@ -64,6 +100,8 @@ public class Knapsack_0_1 {
 
         System.out.println("Memoisation Answer : " + knapsack(val, wt, W, val.length, dp));
 
+
+        System.out.println("Tabulation Answer : " + knapsackTab(val, wt, W));
 
     }    
 }
