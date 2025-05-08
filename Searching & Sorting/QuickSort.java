f5948080400ef553f90e37c718e126b2ac2ba7e5
Dnyanesh Agale <dnyanesh0019@gmail.com>
2025-05-08 08:23:46 +0530

    Quick Sort Updated

--- Searching & Sorting/QuickSort.java
+++ Searching & Sorting/QuickSort.java
@@ -1,55 +1,44 @@
-import java.util.*;
-import java.io.*;
-
 public class QuickSort {
-
-    public static void quicksort(int[] arr, int lo, int hi){
-        if(lo>=hi){
-            return ;
-        }
-        int pivot = arr[hi];
-        int pi = partition(arr, pivot, lo, hi);
-        quicksort(arr, lo, pi - 1);
-        quicksort(arr, pi + 1, hi);
-    }
-
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int N = scn.nextInt();
+        int[] arr = {4, 2, 7, 3, 9, 5};
 
-        int[] arr = new int[N];
-        for(int i=0; i<arr.length; i++){
-            arr[i]=scn.nextInt();
+        quickSort(arr, 0, arr.length-1);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void quickSort(int[] arr, int si, int ei) {
+        if (si >= ei) {
+            return;
         }
 
-        quicksort(arr, 0, arr.length-1);
+        int pivIdx = partition(arr, si, ei);;
+        quickSort(arr, si, pivIdx-1);
+        quickSort(arr, pivIdx+1, ei);
     }
 
-    public static void swap(int[] arr, int i, int j){
-        System.out.println("Swapping "+ arr[i] + " and " + arr[j]);
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public static int partition(int[] arr, int pivot, int lo, int hi){
-        //0 to j-1 -> <= pivot
-        //j to i-1 -> > pivot
-        //i to end -> unknown
+    public static int partition(int[] arr, int si,  int ei) {
+        int pivot = arr[ei];
 
-        System.out.println("pivot -> "+pivot);
-        int i=lo; 
-        int j=lo;
+        int i = si - 1;
 
-        while(i <= hi){
-            if(arr[i]>pivot){
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
                 i++;
-            }else{
-                swap(arr, i, j);
-                i++;
-                j++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
-        System.out.println("Pivot Index -> " + (j-1));
-        return (j-1);
+
+        i++;
+        int temp = arr[ei];
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        
+        return i;
+
     }
 }
